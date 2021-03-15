114c1ff76c24f5ae2a1759c9eb0dadf769fc3f4b
lekjos <leif.kjos@outlook.com>
2021-03-15 00:11:16 -0700

    update sh

--- log_update_watchdog.sh
+++ log_update_watchdog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="/home/***REMOVED***/log/console"
-while inotifywait -m -e modify ~/log/console/vhserver-console.log; do
+inotifywait -m -e modify ~/log/console/vhserver-console.log while read f
+
+do
     echo 'do something';
     cat vhserver-console.log | egrep 'Got handshake from client|Closing socket' > ./recent.log;
     python3 /home/***REMOVED***/source/vhservertools/discord_post.py;
