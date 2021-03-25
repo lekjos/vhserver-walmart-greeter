89b9e4d264f9faf92594c7321beffecad1b4046f
lekjos <leif.kjos@outlook.com>
2021-03-24 19:42:19 -0700

    add lsb header

--- log_update_watchdog.sh
+++ log_update_watchdog.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          filenotifier
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Walmart-greeter shell script
+# Description:       Updates recent.log when changes made to vh log
+### END INIT INFO
+
 DIR="/home/***REMOVED***/log/console"
 inotifywait -m -e modify /home/***REMOVED***/log/console/vhserver-console.log | while read f
 
